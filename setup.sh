3130a88849bb33ba90af21424b7714b14f616034
Brandon Slinkard <slinkardbrandon@gmail.com>
2019-05-09 20:48:46 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+ROOT="$(pwd)"
+
+set -e
+
+main () {
+    "$ROOT/symlink-common.sh"
+
+    if [[ $(uname -s) == "Darwin" ]]; then
+        "$ROOT/config-osx.sh"
+        "$ROOT/install-osx-dev-apps.sh"
+        "$ROOT/config-git.sh"
+        "$ROOT/install-osx-desktop-apps.sh"
+        "$ROOT/symlink-osx-dev-apps.sh"
+    fi
+
+}
+
+main
+
+echo All Done!
+echo Restarting shell
+exec "$(which $SHELL)" -l
