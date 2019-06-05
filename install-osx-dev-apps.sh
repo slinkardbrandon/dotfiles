29fd6d0ed9218e3a9c944525594b2588347929c2
Brandon Slinkard <brandon.slinkard@nelnet.net>
2019-06-05 09:18:56 -0500

    Install brew

--- install-osx-dev-apps.sh
+++ install-osx-dev-apps.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 echo Installing dev apps
 
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Install AWS Cli
 pip3 install awscli --upgrade --user
-
-# Install CMake [https://cmake.org/]
-brew install cmake
