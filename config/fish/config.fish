25198139193d073e91834b8c85ed8f80a263037c
Brandon Slinkard <slinkardbrandon@gmail.com>
2023-02-28 11:12:14 -0600

    feat: VEL-7847 - add some aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,11 @@ set --export --prepend PATH "/Users/slink/.rd/bin"
 starship init fish | source
 
 nvm use lts
+
+alias g="git"
+alias p="pnpm"
+alias k="kubectl"
+alias n="npm"
+alias nv="nvim"
+alias vim="nvim"
+alias f="fuck"
