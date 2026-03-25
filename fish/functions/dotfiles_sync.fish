bbc54c1a0239ef8e14f7e83f4a2e59a56442e389
Brandon Slinkard <slinkardbrandon@gmail.com>
2026-03-25 09:49:13 -0500

    source latest fish config after sync

--- fish/functions/dotfiles_sync.fish
+++ fish/functions/dotfiles_sync.fish
@@ -2,4 +2,8 @@ function dotfiles_sync --description 'Pull latest dotfiles and run sync'
     set -l prev (pwd)
     cd ~/dotfiles && git pull && bun run sync
     cd $prev
+
+    # Reload fish config so new aliases/functions are available immediately
+    source ~/.config/fish/config.fish
+    echo "[✓] Fish config reloaded"
 end
